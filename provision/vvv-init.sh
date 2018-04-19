1898186366ab7a8f274fc7062125314636a3eb3b
Peter Fabian <peter.fabian.github@gmail.com>
2018-04-19 12:11:09 +0200

    Updated config

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -2,9 +2,9 @@
 # Provision WordPress Stable
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database 'wordpress_default' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_default"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_default.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database 'woo_stable' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS woo_stable"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON woo_stable.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -24,9 +24,9 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   cd ${VVV_PATH_TO_SITE}/public_html
 
   echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname=wordpress_default --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  noroot wp core config --dbname=woo_stable --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 // Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.wordpress.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(woostable.woo.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
     define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
     define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
 }
@@ -35,7 +35,7 @@ define( 'WP_DEBUG', true );
 PHP
 
   echo "Installing WordPress Stable..."
-  noroot wp core install --url=local.wordpress.test --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+  noroot wp core install --url=woostable.woo.test --quiet --title="Local WooCommerce Stable Dev" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
 
 else
 
